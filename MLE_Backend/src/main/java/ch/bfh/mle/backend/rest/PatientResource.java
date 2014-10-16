08a2133f801e635493e9cbf49e8625dce58c245f
walls2 <stefan@walle.ch>
2014-10-16 08:24:10 +0200

    Backup files deleted

--- MLE_Backend/src/main/java/ch/bfh/mle/backend/rest/PatientResource.java
+++ MLE_Backend/src/main/java/ch/bfh/mle/backend/rest/PatientResource.java
@@ -6,18 +6,14 @@
 package ch.bfh.mle.backend.rest;
 
 import ch.bfh.mle.backend.model.Patient;
-import ch.bfh.mle.backup.files.GenericJpaDao;
-import ch.bfh.mle.backup.files.IPatientDao;
 import ch.bfh.mle.backend.service.PatientService;
 import java.util.Collection;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
--- MLE_Backend/src/main/java/ch/bfh/mle/backup/files/IPatientDao.java
+++
@@ -1,8 +0,0 @@
-package ch.bfh.mle.backup.files;
-
-import ch.bfh.mle.backend.model.Patient;
-
-
-public interface IPatientDao extends IDao<Patient> {
-	
-}
