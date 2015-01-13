9a4c8819faf03edb9ef94cd1c95926f500a47fe0
walls2 <stefan@walle.ch>
2015-01-13 13:08:33 +0100

    Revert "BackOffice: Create Automatic Activities fixed"
    This reverts commit 308d96bf6c697edd6de2eb576ee75250dea17a79.

--- MLE_Backend/target/generated-sources/annotations/ch/bfh/mle/backend/model/Supplier_.java
+++ MLE_Backend/target/generated-sources/annotations/ch/bfh/mle/backend/model/Supplier_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-13T10:59:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-04T22:01:50")
 @StaticMetamodel(Supplier.class)
 public class Supplier_ { 
 
